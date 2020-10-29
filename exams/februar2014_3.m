59f2d37b6c6c8415978b8f8aadba628eec2b4d78
David Rubin <david.rubin95@gmail.com>
2020-10-29 22:19:40 +0100

    Solved task 3 on exam from feb 2014

--- exams/februar2014_3.m
+++ exams/februar2014_3.m
@@ -1,5 +1,8 @@
 function y = februar2014_3(x)
-    % Enacba za volumen
-    % glej tex kaj so x1,x2,x3,x4
+    % Enacba za volumen, kjer je:
+    % x1 ... h1
+    % x2 ... h2
+    % x3 ... R1
+    % x4 ... R2
     y = pi * (x(1) + x(2)) / 3 * (x(3)^2 + x(4)^2 + x(3)*x(4));
 end
--- exams/februar2014_3_con.m
+++ exams/februar2014_3_con.m
@@ -1,5 +1,4 @@
 function [c, ceq] = februar2014_3_con(x)
-    c = [-x(3) -x(4) + 12 ]; % manjse ali enako imamo v drugem delu naloge
-    % Omejitve za trikotnike (pitagorov izrek)
-    ceq = [x(3)^2 + x(1)^2 - 100; x(4)^2 + x(2)^2 - 100]; %  iammo samo tipa je enako
+    c = [12 - x(3) - x(4)]; % omejitev tipa manjse ali enako imamo v drugem delu naloge (za prvi del pustimo prazno)
+    ceq = [x(3)^2 + x(1)^2 - 100; x(4)^2 + x(2)^2 - 100]; % omejitev tipa enako iz prvega dela (maks. stozec)
 end
